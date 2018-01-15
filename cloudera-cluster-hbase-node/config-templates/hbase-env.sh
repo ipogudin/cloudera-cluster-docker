a5cdfe72ff4572e81ce90acd2c8b5f189a2b8f4d
Ivan Pogudin <i.a.pogudin@gmail.com>
2018-01-15 21:12:24 +0600

    Added explicit heap parameters for all java processes.

--- cloudera-cluster-hbase-node/config-templates/hbase-env.sh
+++ cloudera-cluster-hbase-node/config-templates/hbase-env.sh
@@ -30,7 +30,7 @@
 # export HBASE_CLASSPATH=
 
 # The maximum amount of heap to use. Default is left to JVM default.
-# export HBASE_HEAPSIZE=1G
+export HBASE_HEAPSIZE={{ default .Env.HBASE_ENV_HBASE_HEAPSIZE "1G" }}
 
 # Uncomment below if you intend to use off heap cache. For example, to allocate 8G of 
 # offheap, set the value to "8G".
